cf9a974e30233c0e2f16dc9b811546c9b1f391e6
T <tyson_ngo@hotmail.com>
2022-01-23 06:45:32 -0500

    build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+python3 -m PyInstaller --name=UMvC3_Stage_Installer --onefile --icon=icon\icon.ico main.pyw
+python3 -m PyInstaller --name=umvc3stg_file_generator --onefile --icon=icon\icon.ico generate_umvc3stg.pyw
